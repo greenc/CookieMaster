8dc5c2f25a92566997bc14793b29bc3772ea60dc
Chris Green <c.robert.green@gmail.com>
2014-02-04 11:25:59 +0000

    Ported the refactored bootstrap file over to the Chrome extension inject script, also tidied up the manifest file

--- chrome-extension/inject.js
+++ chrome-extension/inject.js
@@ -1,51 +1,85 @@
+/*global chrome:false */
+
 chrome.extension.sendMessage({}, function(response) {
 
 	var readyStateCheckInterval = setInterval(function() {
 
-		if (document.readyState === "complete") {
+		if (document.readyState === 'complete') {
 
 			clearInterval(readyStateCheckInterval);
 
-			// Create script elements
-			var cmcss     = document.createElement('link'),
-				uien      = document.createElement('link'),
-				jquery    = document.createElement('script'),
-				jsapi     = document.createElement('script'),
-				cmex      = document.createElement('script'),
-				cmjs      = document.createElement('script'),
-				corechart = document.createElement('script');
+			var docFrag = document.createDocumentFragment(),
+				deps    = [
+					{
+						// CookieMaster CSS
+						type:  'link',
+						url:   'build/cookiemaster.css',
+						async: true
+					},
+					{
+						// Google Chart CSS
+						type:  'link',
+						url:   'lib/ui+en.css',
+						async: true
+					},
+					{
+						// Google Charts API
+						type:  'script',
+						url:   'lib/jsapi.js',
+						async: false
+					},
+					{
+						// Core Chart API
+						type:  'script',
+						url:   'lib/corechart.js',
+						async: false
+					},
+					{
+						// jQuery
+						type:  'script',
+						url:   'lib/jquery-2.1.0.min.js',
+						async: false
+					},
+					{
+						// External CookieMaster methods
+						type:  'script',
+						url:   'build/external-methods.js',
+						async: false
+					},
+					{
+						// CookieMaster methods
+						type:  'script',
+						url:   'build/cookiemaster.js',
+						async: false
+					}
+				],
+				dep, el, i;
 
-			// Set CSS file attributes
-			cmcss.rel  = 'stylesheet';
-			cmcss.type = 'text/css';
-			cmcss.href = chrome.extension.getURL('build/cookiemaster.css');
+			/**
+			 * Create an element for each dependency and
+			 * append to a document fragment
+			 */
+			for(i = 0; i < deps.length; i++) {
 
-			uien.rel  = 'stylesheet';
-			uien.type = 'text/css';
-			uien.href = chrome.extension.getURL('lib/ui+en.css');
+				dep = deps[i];
+				el  = document.createElement(dep.type);
 
-			// Set async on scripts
-			jquery.async    = false;
-			jsapi.async     = false;
-			cmex.async      = false;
-			cmjs.async      = false;
-			corechart.async = false;
+				if(dep.type === 'link') {
+					el.rel  = 'stylesheet';
+					el.href = dep.url;
+				} else if(dep.type === 'script') {
+					el.src = dep.url;
+				}
 
-			// Set script sources
-			jquery.src     = chrome.extension.getURL('lib/jquery-2.1.0.min.js');
-			jsapi.src      = chrome.extension.getURL('lib/jsapi.js');
-			cmex.src       = chrome.extension.getURL('build/external-methods.js');
-			cmjs.src       = chrome.extension.getURL('build/cookiemaster.js');
-			corechart.src  = chrome.extension.getURL('lib/corechart.js');
+				el.async = dep.async;
+				docFrag.appendChild(el);
 
-			// Append the scripts
-			(document.head||document.documentElement).appendChild(cmcss);
-			(document.head||document.documentElement).appendChild(uien);
-			(document.body||document.documentElement).appendChild(jsapi);
-			(document.body||document.documentElement).appendChild(corechart);
-			(document.body||document.documentElement).appendChild(jquery);
-			(document.body||document.documentElement).appendChild(cmex);
-			(document.body||document.documentElement).appendChild(cmjs);
+			}
+
+			/**
+			 * Append fragment to DOM
+			 */
+			document.head.appendChild(docFrag);
 
 		}
 
