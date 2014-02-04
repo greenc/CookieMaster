ae293b6cf73d7f1457b793970e488a0df0c6e059
Sam Nichols <samboni@ymail.com>
2014-02-03 19:24:50 -0600

    Added the GreaseMonkey Script
    Placed the file in a new folder titled "GreaseMonkey Script". Just run
    this file with GreaseMonkey already installed on FireFox to add the
    script.

---
+++ GreaseMonkey Script/CookieMonster.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Cookie Master
+// @namespace   http://cookiemaster.co.uk/
+// @description An addon for Cookie Clicker
+// @include     http://orteil.dashnet.org/cookieclicker/*
+// @version     1.10.3
+// ==/UserScript==
+
+javascript:(function(d){d.body.appendChild(d.createElement('script')).setAttribute('src','//cookiemaster.co.uk/build/cm-bootstrap.min.js?cb='+new Date().getTime())})(document)
