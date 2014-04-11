d256dab2870bb907d1972bc8ab2301536637ed0f
RashFael <rashfael@isobeef.org>
2014-04-11 23:51:48 +0200

    add userscript

--- userscript/cookiemaster.user.js
+++ userscript/cookiemaster.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name        CookieMaster
-// @namespace   http://cookiemaster.co.uk
+// @namespace   http://cookiemaster.creatale.de
 // @description A feature-rich plugin for Cookie Clicker
 // @version     1.0.1
 // @include     http://orteil.dashnet.org/cookieclicker/
 // @exclude     http://orteil.dashnet.org/cookieclicker/beta/
-// @downloadURL http://cookiemaster.co.uk/userscript/cookiemaster.user.js
-// @updateURL   http://cookiemaster.co.uk/userscript/cookiemaster.user.js
-// @icon        http://cookiemaster.co.uk/site/images/cookiemaster.icon.user.png
+// @downloadURL http://cookiemaster.creatale.de/userscript/cookiemaster.user.js
+// @updateURL   http://cookiemaster.creatale.de/userscript/cookiemaster.user.js
+// @icon        http://cookiemaster.creatale.de/site/images/cookiemaster.icon.user.png
 // ==/UserScript==
 
 (function(d){
     d.head
         .appendChild(d.createElement('script'))
-        .setAttribute('src','//cookiemaster.co.uk/b?c='+new Date().getTime());
+        .setAttribute('src','//cookiemaster.creatale.de/b?c='+new Date().getTime());
 })(document);
